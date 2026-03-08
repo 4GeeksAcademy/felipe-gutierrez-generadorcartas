200223ee9f80f0058e6b0fc98e502efb81942689
felipenehuengutierrez <felipe.nehueng@gmail.com>
2026-03-08 22:00:11 +0000

    generadordecartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+
+ 
+  let indicePinta = Math.floor(Math.random() * 4);
+  let indiceNumero = Math.floor(Math.random() * 13);
+
+
+  let pintas = ["♦", "♥", "♠", "♣"];
+  let clasesPintas = ["diamond", "heart", "spade", "club"];
+  let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+
+  let carta = document.getElementById("carta");
+  let pintaArriba = document.getElementById("pinta-arriba");
+  let pintaAbajo = document.getElementById("pinta-abajo");
+  let numeroCarta = document.getElementById("numero-carta");
+
+  
+  pintaArriba.innerHTML = pintas[indicePinta];
+  pintaAbajo.innerHTML = pintas[indicePinta];
+  numeroCarta.innerHTML = numeros[indiceNumero];
+
+
+  carta.classList.add(clasesPintas[indicePinta]);
 };
